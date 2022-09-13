1db3b0a1cf6937b79e4963b77654719d0b6980f6
Jin Igarashi <igarashi.jin@water-gis.com>
2022-09-13 18:25:53 +0100

    deleted json file from repo

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,7 @@ export const config = {
 	url: 'http://cactuscosting.com',
 	logo: `${process.env.NODE_ENV === 'production' ? '/cactus' : ''}/favicon.png`,
 	base_worldbank_api: 'https://api.worldbank.org/v2/country',
-	// cactus_api: 'https://cactuscosting.com/cactus02/cactus-calcs/pull_cactus_json_app_json.php'
-	cactus_api: './all_cactus_datapoint_data.json',
+	cactus_api: 'https://cactuscosting.com/cactus02/cactus-calcs/pull_cactus_json_app_json.php',
 	attribution:
 		'&copy; <a href="https://cactuscosting.com" target="_blank" rel="noopener">CACTUS project</a>'
 };
